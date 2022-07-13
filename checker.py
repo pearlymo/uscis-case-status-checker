3ee4cfdcea49ae804ac6118fb4e0839469ec8389
pearlymo <mothi.ghimire@gmail.com>
2022-07-13 14:53:58 -0600

    checker: add comments and replace single quotes with doubles

--- checker.py
+++ checker.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+# Add path to Chrome's webdriver in the Chrome parameter
+driver = webdriver.Chrome("")
 
-driver.get('https://egov.uscis.gov/casestatus/landing.do')
+driver.get("https://egov.uscis.gov/casestatus/landing.do")
 
 # Add your receipt in the send_keys parameter
-driver.find_element(By.ID, 'receipt_number').send_keys('')
-driver.find_element(By.NAME, 'initCaseSearch').click()
+driver.find_element(By.ID, "receipt_number").send_keys("")
+driver.find_element(By.NAME, "initCaseSearch").click()
